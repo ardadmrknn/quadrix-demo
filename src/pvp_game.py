f5063b6ecd4ebeaa9be147a0fa87e3d69a524d0e
Burak Yaşayan <yasayanburak@gmail.com>
2026-04-20 09:55:44 +0300

    feat(gamepad): Add rumble feedback on piece lock for improved player experience

--- src/pvp_game.py
+++ src/pvp_game.py
@@ -1775,6 +1775,12 @@ class PvPGame:
 
                         self.lock_and_new_piece(1)
 
+                            try:
+                                from gamepad_manager import get_gamepad_manager
+                                get_gamepad_manager().rumble(0.3, 0.6, 120)
+                            except Exception:
+                                pass
+
                     # Hold
                     elif event.key == controls1['hold']:
                         self._try_hold_piece(1)
@@ -1851,6 +1857,12 @@ class PvPGame:
                             self.trigger_hard_drop_screen_shake()
 
                         self.lock_and_new_piece(2)
+
+                            try:
+                                from gamepad_manager import get_gamepad_manager
+                                get_gamepad_manager().rumble(0.3, 0.6, 120)
+                            except Exception:
+                                pass
 
                     # Hold
                     elif event.key == controls2['hold']:
